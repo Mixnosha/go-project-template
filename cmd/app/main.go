4e4eaf6c4af5553b47d5954f4fa87194af13b7d8
Mixnosha <xmixho@gmail.com>
2024-06-27 11:12:35 +0300

    create dirs if not exist

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -45,6 +45,20 @@ func setupLogger(env string) *slog.Logger {
 func setupFileOutputSlog(
 	hadlerOptions *slog.HandlerOptions,
 ) *slog.Logger {
+    if _, err := os.Stat("data"); os.IsNotExist(err) {
+        err := os.Mkdir("data", 0755)
+        if err != nil{
+            panic("Error creating data dir: " + err.Error())
+        }
+    }
+
+    if _, err := os.Stat("data/logs"); os.IsNotExist(err) {
+        err := os.Mkdir("data/logs", 0755)
+        if err != nil{
+            panic("Error creating data/logs dir: " + err.Error())
+        }
+    }
+
 	writer := jsonwrt.NewJsonWriter("data/logs/")
 	log := slog.New(
 		slog.NewJSONHandler(writer, hadlerOptions),
